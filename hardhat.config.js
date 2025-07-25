d6b3f94827da8ec2f386489a43c7e337c734a93e
Bruno Moretti <brmoretti@gmail.com>
2025-07-24 23:34:11 -0300

    feat: add network configurations for Avalanche Mainnet and Fuji Testnet in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
 	solidity: {
@@ -10,6 +11,26 @@ module.exports = {
 				enabled: true,
 				runs: 200
 			}
+		}
+	},
+	networks: {
+		// Local development
+		hardhat: {
+			chainId: 31337
+		},
+		// Avalanche Mainnet
+		avalanche: {
+			url: "https://api.avax.network/ext/bc/C/rpc",
+			gasPrice: 225000000000,
+			chainId: 43114,
+			accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+		},
+		// Avalanche Fuji Testnet (recommended for testing)
+		fuji: {
+			url: "https://api.avax-test.network/ext/bc/C/rpc",
+			gasPrice: 225000000000,
+			chainId: 43113,
+			accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
 		}
 	},
 	paths: {
